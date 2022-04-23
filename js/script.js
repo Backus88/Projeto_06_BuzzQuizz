19ee4beada45ee620857f6cfcab1cc78c919d670
Jefferson Oliveira <jeffebido@hotmail.com>
2022-04-22 21:22:07 -0300

    Gerando perguntas

--- js/script.js
+++ js/script.js
@@ -2,6 +2,84 @@ const linksQuizz = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
 
 const perguntasArray = [];
 const infoObjeto ={};
+let infoBasicasQUizz={};
+
+function validaInfoBasicas() {
+    
+
+
+    infoBasicasQUizz = {
+        nome: document.getElementsByName('quizz-titulo')[0].value,
+        img: document.getElementsByName('quizz-img')[0].value,
+        qtdPerguntas: document.getElementsByName('quizz-qtd-perguntas')[0].value,
+        qtdNiveis: document.getElementsByName('quizz-qtd-niveis')[0].value
+    };
+
+    document.querySelector(".step-1").classList.add("display-none");
+    renderizaPerguntas();
+}
+
+function renderizaPerguntas() {
+
+    const corpoNivel = document.querySelector(".step-2 .creation-body");
+    corpoNivel.innerHTML = ``;
+    for (let index = 0; index < infoBasicasQUizz.qtdPerguntas; index++) {
+      corpoNivel.innerHTML += `
+        
+        <div class="perguntas-body">
+            <div role="button" class="hidden-menu" onclick="alternaPerguntas(this)">
+                <h2>Pergunta ${index +1}</h2>
+                <img class ="img-editar" src="imgs/editIcon.svg">
+            </div>
+            <div class="inputs">
+                <input type="text" placeholder="Texto da pergunta" name="pergunta-${index}-texto"></input>
+                <input type="text" placeholder="Cor de fundo da pergunta" name="pergunta-${index}-"></input>
+                <hr>
+                <h2>Resposta correta</h2>
+                <input type="text" placeholder="Resposta correta" name="pergunta-${index}-resposta-correta"></input>
+                <input type="text" placeholder="URL da imagem" name="pergunta-${index}-url-imagem"></input>
+                <hr>
+                <h2>Respostas incorretas</h2>
+                <input type="text" placeholder="Resposta incorreta 1" name="pergunta-${index}-resposta-incorreta-1"></input>
+                <input type="text" placeholder="URL da imagem 1" name="pergunta-${index}-url-imagem-incorreta-1"></input>
+                <hr>
+                <input type="text" placeholder="Resposta incorreta 1" name="pergunta-${index}-resposta-incorreta-2"></input>
+                <input type="text" placeholder="URL da imagem 1" name="pergunta-${index}-url-imagem-incorreta-2"></input>
+                <hr>
+                <input type="text" placeholder="Resposta incorreta 1" name="pergunta-${index}-resposta-incorreta-3"></input>
+                <input type="text" placeholder="URL da imagem 1" name="pergunta-${index}-url-imagem-incorreta-3"></input>
+            </div>
+        </div>
+        `;
+    }
+
+    document.querySelector(".step-2").classList.remove("display-none");
+
+    
+}
+function alternaPerguntas(elemento){
+
+    elemento = elemento.parentNode;
+
+
+    let selecionado = document.querySelector(".perguntas-body.active");
+
+    if (selecionado == null) {
+  
+      elemento.classList.add("active");
+  
+    }else{
+      
+      selecionado.classList.remove("active");
+      elemento.classList.add("active"); 	
+    }
+}
+function validaPerguntas(){
+    
+    document.querySelector(".step-2").classList.add("display-none");
+
+    renderizaNiveis();
+}
 
 // função alterna estado do nível
 function alternaNiveis(elemento) {
@@ -42,14 +120,11 @@ function alternaNiveis(elemento) {
   }
 }
 
-
 function renderizaNiveis() {
-  // para simular o recebimento do numero de niveis
-  let numeroNiveis = 2;
-  //
+ 
   const corpoNivel = document.querySelector(".step-3 .creation-body");
   corpoNivel.innerHTML = ``;
-  for (let index = 0; index < numeroNiveis; index++) {
+  for (let index = 0; index < infoBasicasQUizz.qtdNiveis; index++) {
     corpoNivel.innerHTML += `
     <div class="nivel-body">
         <div role="button" class="hidden-menu" onclick="alternaNiveis(this)">
@@ -65,9 +140,10 @@ function renderizaNiveis() {
       </div>   
       `;
   }
+
+  document.querySelector(".step-3").classList.remove("display-none");
 }
 
-renderizaNiveis();
 
 //funções para validar os campos do nível
 const verificaTamanhoTitulo = titulo => titulo.length >= 10;
